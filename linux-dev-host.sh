a550929bc1983ab2cc4d65b5c37dcc091147bd53
Frédéric ROLLAND <frederic.rolland@free.fr>
2021-08-17 22:33:01 +0200

    aide installation packages adhoc

---
+++ linux-dev-host.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install open-vm-tools open-vm-tools-desktop
+sudo apt-get install build-essential gdb clazy cppcheck csstidy tidy weblint-perl valgrind kcachegrind
+sudo apt-get install sysvbanner cowsay htop meld kate gedit snapd nodejs npm cmake
+sudo apt-get install tree pv dialog wget curl p7zip sqlite3 curl terminator terminology rrdtool rsync cool-retro-term konsole
+sudo apt-get install docker docker-compose 
+sudo apt-get install git git-lfs gitk git-gui git-cola qgit 
+sudo apt-get install gparted inkscape gimp krita digikam gthumb thunar 
+sudo apt-get install python3 python3-pip python3-venv python3-bs4 python3-serial python3-mysql.connector python3-mysqldb
+sudo apt-get install kdevelop qtcreator pyqt5-dev
+
+sudo -H python3 -m pip install pip --upgrade
+sudo -H python3 -m pip install setuptools --upgrade
+
+sudo snap install gitkraken
+sudo snap install code
+sudo snap install pycharm-community
